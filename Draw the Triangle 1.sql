791a9ef506ff87a96f6c3166e1d465d945daade4
Devanshi <126303161+Devanshi-20@users.noreply.github.com>
2026-04-03 20:29:12 -0400

    Add files via upload

---
+++ Draw the Triangle 1.sql
@@ -0,0 +1,17 @@
+P(R) represents a pattern drawn by Julia in R rows. The following pattern represents P(5):
+
+* * * * * 
+* * * * 
+* * * 
+* * 
+*
+Write a query to print the pattern P(20).
+
+
+WITH RECURSIVE t(n) AS (
+    SELECT 20
+    UNION ALL
+    SELECT n - 1 FROM t WHERE n > 1
+)
+SELECT REPEAT('* ', n)
+FROM t;
